419b2b37f57ab47db86916e910e05531fce6ffbc
Chenqi <1002652154@qq.com>
2021-03-04 17:20:40 +0800

    dynamic store interface completed

--- code/DynamicStore/engine.h
+++ code/DynamicStore/engine.h
@@ -2,47 +2,93 @@
 
 #include "core.h"
 
-#include <array>
-#include <vector>
-
 
 BEGIN_NAMESPACE(DynamicStore)
 
 using Index = uint64;
-using Length = uint64;
-
 constexpr Index invalid_index = (Index)-1;
 
 
 class Engine {
 public:
+	Engine() {}
+	~Engine() {}
 
-	bool IsEmpty() {
+public:
+	void Load(const wchar file[]) {}
+
+	template<class Metadata>
+	void GetMetadata(Metadata& metadata) {
+		// Metadata should implement Check and Init function.
+
+		// First check metadata size.
+
+		// If size match, load metadata and Check.
+
+		// If check fails, initialize metadata.
+
 
 	}
 
-	template<class ElementType>
-	Index CreateArray(ref_ptr<ElementType> data, Length length) {
-
+	template<class Metadata>
+	Metadata GetMetadata() {
+		Metadata metadata; GetMetadata(metadata);
+		return metadata;
 	}
+
+public:
+	Index CreateArray() { return 0; }
+	void DestroyArray(Index index) {}
+
+	uint64 GetArraySize(Index index) { return 0; }
+	void SetArraySize(Index index, uint64 size) {}
 };
 
 
-template<class _ElementType>
+template<class ElementType>
 class Array {
 private:
-	using ElementType = _ElementType == Array ? Index : _ElementType;
-
-
-private:
-	Engine& engine;
-	Index index;
-private:
 	static constexpr uint64 element_size = sizeof(ElementType);
-	Length length;
+private:
+	Engine& _engine;
+	Index _index;
+	uint64 _length;
 public:
-	Array(Engine& engine) : engine(engine), index(invalid_index), length(0) {}
-	Array(Engine& engine, Index index, Length length) : engine(engine), index(index), length(length) {}
+	Array(Engine& engine, Index index) : _engine(engine), _index(index) {
+		uint64 size = engine.GetArraySize(index);
+		assert(size % element_size == 0);
+		_length = size / element_size;
+	}
+
+	uint64 GetLength() { return _length; }
+
+	template<class OutputIterator>
+	void Load(uint64 begin, uint64 length, OutputIterator it) {
+		if (length == 0) { return; }
+		if (begin + length > _length) { throw std::out_of_range("subscript out of range"); }
+	}
+
+	template<class OutputIterator>
+	void Load(OutputIterator it) { 
+		Load(0, _length, it);
+	}
+
+
+	void SetLength(uint64 length) {
+		_engine.SetArraySize(_index, length * element_size);
+		_length = length;
+	}
+
+	template<class InputIterator>
+	void Store(InputIterator it, uint64 begin, uint64 length) {
+		if (length == 0) { return; }
+		if (begin + length > _length) { throw std::out_of_range("subscript out of range"); }
+	}
+
+	template<class InputIterator>
+	void Store(InputIterator it) {
+		Store(it, 0, _length);
+	}
 };
 
 
--- code/Test/test.h
+++ code/Test/test.h
@@ -3,33 +3,86 @@
 #include "../DynamicStore/engine.h"
 
 #include <string>
+#include <list>
+#include <vector>
+#include <iterator>
 
 
 using namespace DynamicStore;
 
 using std::wstring;
+using std::list;
+using std::vector;
 
 
-class Paragraph : public Array<wchar> {
+Engine engine;
 
+struct ListData {
+	Index list_index;
+	bool Check() const {
+		return list_index != invalid_index;
+	}
+	void Init(Engine& engine) {
+		list_index = engine.CreateArray();
+	}
 };
 
 
-class ParagraphList : public Array<Paragraph> {
-	
+class TextEditor /*: public ListLayout*/ {
+private:
+	struct ParagraphData {
+		unsigned int indent_level = 0;
+		char32_t label = U'\0';
+		Index text_array_index = invalid_index;
+	};
 
-};
-
-
-class StoreEngine : public Engine {
+	class Paragraph /*: public TextBox*/ {
+	private:
+		friend class TextEditor;
+		list<Paragraph>::iterator _index;
+		ParagraphData _data;
+		Array<wchar> _text_array;
+		///*
+		wstring _text;
+		wstring& GetText() { return _text; }
+		//*/
+	public:
+		Paragraph(ParagraphData data): _data(data), _text_array(engine, data.text_array_index) {
+			_text_array.Load(0, _text_array.GetLength(), std::back_inserter(GetText()));
+		}
+		~Paragraph() {
+			_text_array.SetLength(GetText().length());
+			_text_array.Store(GetText().begin(), 0, _text_array.GetLength());
+		}
+		operator ParagraphData() { return _data; }
+	};
+private:
+	list<Paragraph> _paragraph_list;
+	Array<ParagraphData> _paragraph_array;
 public:
-
+	TextEditor(ListData data) : _paragraph_array(engine, data.list_index) {
+		vector<ParagraphData> vector; vector.reserve(_paragraph_array.GetLength());
+		_paragraph_array.Load(std::back_inserter(vector));
+		for (auto& data : vector) {
+			_paragraph_list.emplace_back(data);
+			_paragraph_list.back()._index = --_paragraph_list.end();
+		}
+		// Create the first paragraph if there's no paragraph.
+		if (_paragraph_list.empty()) {
+			ParagraphData data;
+			data.text_array_index = engine.CreateArray();
+			_paragraph_list.emplace_back(data);
+		}
+	}
+	~TextEditor() {
+		_paragraph_array.SetLength(_paragraph_list.size());
+		_paragraph_array.Store(_paragraph_list.begin());
+	}
 };
 
 
 int main() {
-	StoreEngine engine(L"text.dat");
-	if (engine.IsEmpty()) {
-		engine.CreateArray()
-	}
+	engine.Load(L"text.dat");
+	ListData list_data = engine.GetMetadata<ListData>();
+	TextEditor text_editor(list_data);
 }
